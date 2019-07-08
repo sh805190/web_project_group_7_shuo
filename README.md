824f571be195418c408d933ecd958653bada279d
sh805190 <qwerqazqqq@gmail.com>
2019-07-08 20:08:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-y7rijq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-y7rijq)
---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { AvailableRoomComponent } from './available-room/available-room.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BookingsComponent } from './bookings/bookings.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { ConfirmEqualValidatorDirective } from './confirm-equal-validator.directive';
+import { HttpClientModule } from '@angular/common/http';
+import { SearchFilterComponent } from './search-filter/search-filter.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HotelDetailsComponent } from './hotel-details/hotel-details.component';
+import { BookingRecordsComponent } from './booking-records/booking-records.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignupComponent,
+    LoginComponent,
+    AvailableRoomComponent,
+    ProfileComponent,
+    BookingsComponent,
+    FeedbackComponent,
+    ContactUsComponent,
+    HomeComponent,
+    ConfirmEqualValidatorDirective,
+    SearchFilterComponent,
+    HotelDetailsComponent,
+    BookingRecordsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
